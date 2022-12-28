e1713d570db46f5d392dbb4246038f63c5d4d368
barsu5489 <ebarsulai@gmail.com>
2022-12-28 20:22:32 +0300

    classes in javascript

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code here
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+class Dinner{
+    #desert;
+    constructor(salad, soup, entree, desert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#desert = desert
+    }
+}
+
+
+
+
+
+// class Dog{
+//     constructor(name,breed){
+//         this.name = name;
+//         this.breed = breed
+//     }
+// }
+
+// let bigfluffyDog1 = new Dog('Roxy','Dogy Safi');
+// let bigfluffyDog2 = new Dog('simba','yaKibonge')
+// console.log(bigfluffyDog1.name, `, `+  bigfluffyDog2.name)
